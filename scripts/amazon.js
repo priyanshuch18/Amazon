f469da6c53cabd39e5b2f69491991e6863a6189c
priyanshu <gurjarp662@gmail.com>
2024-02-21 11:51:51 +0530

    data card more interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product)=>{
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class = "js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -43,7 +43,7 @@ products.forEach((product)=>{
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
       <img src="images/icons/checkmark.png">
       Added
     </div>
@@ -58,7 +58,26 @@ products.forEach((product)=>{
 
 
 document.querySelector('.js-products-grid').innerHTML = productHTML;
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    // This solution uses a feature of JavaScript called a
+    // closure. Each time we run the loop, it will create
+    // a new variable called addedMessageTimeoutId and do
+    // button.addEventListener().
+    //
+    // Then, because of closure, the function we give to
+    // button.addEventListener() will get a unique copy
+    // of the addedMessageTimeoutId variable and it will
+    // keep this copy of the variable forever.
+    // (Reminder: closure = if a function has access to a
+    // value/variable, it will always have access to that
+    // value/variable).
+    //
+    // This allows us to create many unique copies of the
+    // addedMessageTimeoutId variable (one for every time
+    // we run the loop) so it lets us keep track of many
+    // timeoutIds (one for each product).
+    let addedMessageTimeoutId;
     button.addEventListener('click', ()=>{
         const productId = button.dataset.productId;
 
@@ -69,17 +88,40 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             }
         });
 
+        const quantitySelector = document.querySelector(
+            `.js-quantity-selector-${productId}`
+          );
+          const quantity = Number(quantitySelector.value);
+
         if (matchingItem){
-            matchingItem.quantity +=1;
+            matchingItem.quantity += quantity;
         }
         else{
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: quantity
             })
 
         }
         let cartQuantity = 0;
+        const addedMessage = document.querySelector(
+            `.js-added-to-cart-${productId}`);
+            addedMessage.classList.add('added-to-cart-visible');
+            setTimeout(() => {
+                addedMessage.classList.remove('added-to-cart-visible');
+                // Check if a previous timeoutId exists. If it does,
+                // we will stop it.
+                if (addedMessageTimeoutId) {
+                    clearTimeout(addedMessageTimeoutId);
+                }
+
+                const timeoutId = setTimeout(() => {
+                    addedMessage.classList.remove('added-to-cart-visible');
+                }, 2000);
+
+                // Save the timeoutId so we can stop it later.
+                addedMessageTimeoutId = timeoutId;
+              }, 2000);
 
         cart.forEach((item)=>{
             cartQuantity += item.quantity;
