aff021a10205bb512a9959531df50fc129575b6d
priyanshu <gurjarp662@gmail.com>
2024-02-22 19:40:59 +0530

    make delivery option interactive

--- data/cart.js
+++ data/cart.js
@@ -60,3 +60,17 @@ export function addToCart(productId){
     saveToStorage();
 
   }
+
+  export function updateDeliveryOption(productId,deliveryOptionId){
+    let matchingItem;
+          cart.forEach((cartItem)=> {
+              if(productId=== cartItem.productId){
+                  matchingItem =cartItem;
+              }
+          });
+  
+
+          matchingItem.deliveryOptionId = deliveryOptionId;
+          saveToStorage();
+
+  }
