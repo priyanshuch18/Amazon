35943ea3c6abe668bd2b96ef826755ec43c62153
priyanshu <gurjarp662@gmail.com>
2024-02-22 08:53:43 +0530

    add modules and make functions like addToCart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productHTML = '';
@@ -60,77 +60,50 @@ products.forEach((product)=>{
 
 document.querySelector('.js-products-grid').innerHTML = productHTML;
 
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-    // This solution uses a feature of JavaScript called a
-    // closure. Each time we run the loop, it will create
-    // a new variable called addedMessageTimeoutId and do
-    // button.addEventListener().
-    //
-    // Then, because of closure, the function we give to
-    // button.addEventListener() will get a unique copy
-    // of the addedMessageTimeoutId variable and it will
-    // keep this copy of the variable forever.
-    // (Reminder: closure = if a function has access to a
-    // value/variable, it will always have access to that
-    // value/variable).
-    //
-    // This allows us to create many unique copies of the
-    // addedMessageTimeoutId variable (one for every time
-    // we run the loop) so it lets us keep track of many
-    // timeoutIds (one for each product).
-    let addedMessageTimeoutId;
-    button.addEventListener('click', ()=>{
-        const productId = button.dataset.productId;
 
-        let matchingItem;
-        cart.forEach((item)=> {
-            if(productId=== item.productId){
-                matchingItem =item;
-            }
-        });
+function updateCartQuantity(productId){
+  let addedMessageTimeoutId;
+  let cartQuantity = 0;
+      const addedMessage = document.querySelector(
+          `.js-added-to-cart-${productId}`);
+          addedMessage.classList.add('added-to-cart-visible');
+          setTimeout(() => {
+              addedMessage.classList.remove('added-to-cart-visible');
+              // Check if a previous timeoutId exists. If it does,
+              // we will stop it.
+              if (addedMessageTimeoutId) {
+                  clearTimeout(addedMessageTimeoutId);
+              }
 
-        const quantitySelector = document.querySelector(
-            `.js-quantity-selector-${productId}`
-          );
-          const quantity = Number(quantitySelector.value);
-
-        if (matchingItem){
-            matchingItem.quantity += quantity;
-        }
-        else{
-            cart.push({
-                productId: productId,
-                quantity: quantity
-            })
-
-        }
-        let cartQuantity = 0;
-        const addedMessage = document.querySelector(
-            `.js-added-to-cart-${productId}`);
-            addedMessage.classList.add('added-to-cart-visible');
-            setTimeout(() => {
-                addedMessage.classList.remove('added-to-cart-visible');
-                // Check if a previous timeoutId exists. If it does,
-                // we will stop it.
-                if (addedMessageTimeoutId) {
-                    clearTimeout(addedMessageTimeoutId);
-                }
-
-                const timeoutId = setTimeout(() => {
-                    addedMessage.classList.remove('added-to-cart-visible');
-                }, 2000);
-
-                // Save the timeoutId so we can stop it later.
-                addedMessageTimeoutId = timeoutId;
+              const timeoutId = setTimeout(() => {
+                  addedMessage.classList.remove('added-to-cart-visible');
               }, 2000);
 
-        cart.forEach((item)=>{
-            cartQuantity += item.quantity;
+              // Save the timeoutId so we can stop it later.
+              addedMessageTimeoutId = timeoutId;
+            }, 2000);
 
-        });
+      cart.forEach((cartItem)=>{
+          cartQuantity += cartItem.quantity;
 
-        
-        document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+      });
+    
+
+      
+      document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+}
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    
+  
+  button.addEventListener('click', ()=>{
+    const productId = button.dataset.productId;
+      addToCart(productId);
+
+      updateCartQuantity(productId);
+
+  
+      
 
         
         
